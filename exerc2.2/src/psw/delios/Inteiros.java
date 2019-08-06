4fbf4b626782871bf8ad5b9a6b43e6e02935f70f
adelio <adelioa.c@gmail.com>
2019-08-06 20:06:54 -0300

    exercicio 2.2

---
+++ exerc2.2/src/psw/delios/Inteiros.java
@@ -0,0 +1,17 @@
+package psw.delios;
+
+public class Inteiros {
+	int x;
+	int y;
+	
+	public int retornaSoma(int x,int y){
+		int soma= x+y;
+		return soma;
+		
+	}
+	public int retornaDiferenca(int x, int y){
+		int dif= x-y;
+		return dif;
+	}
+
+}
---
+++ exerc2.2/src/psw/delios/Inteiros2.java
@@ -0,0 +1,29 @@
+package psw.delios;
+import java.util.Scanner;
+public class Inteiros2 {
+	
+	
+	// metodo para leitura
+	
+ public static void main( String[] args ){
+	Scanner sc = new Scanner (System.in);
+	Inteiros obj = new Inteiros();
+	
+	obj.x = sc.nextInt();
+	obj.y = sc.nextInt();
+
+	
+	System.out.printf( "o valor de x e:  %d \n o valor de y e: %d \n",obj.x,obj.y);
+	System.out.printf("a soma e %d \n ",obj.retornaSoma(obj.x,obj.y));
+	System.out.printf(" a diferenca e %d \n",obj.retornaDiferenca(obj.x,obj.y));
+	
+	
+	
+	
+	
+	
+	
+}
+
+	
+}

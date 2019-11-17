39bc2b229269e3eb06010f2b2028a9f7bc269170
adelio <adelioa.c@gmail.com>
2019-11-17 01:13:24 -0200

    exer15.5

---
+++ interface/src/desios/psw/exer_15_5/Aplicacao_Circulo.java
@@ -0,0 +1,10 @@
+package desios.psw.exer_15_5;
+
+public class Aplicacao_Circulo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ interface/src/desios/psw/exer_15_5/Exerc_15_5_circulo.java
@@ -0,0 +1,5 @@
+package desios.psw.exer_15_5;
+
+public class Exerc_15_5_circulo {
+
+}
